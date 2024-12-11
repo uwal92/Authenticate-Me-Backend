abf195501e900278cf29d45a459e22ad1db3d123
samrawit <samri.berih@gmail.com>
2024-12-11 11:54:53 -0800

    Add build script to backend package.json

--- backend/psql-setup-script.js
+++ backend/psql-setup-script.js
@@ -4,4 +4,6 @@ sequelize.showAllSchemas({ logging: false }).then(async (data) => {
   if (!data.includes(process.env.SCHEMA)) {
     await sequelize.createSchema(process.env.SCHEMA);
   }
+}).catch((error) => {
+  console.error('Error setting up the schema:', error);
 });
