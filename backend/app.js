61c797f0b383b71a87a2ff2a8a2c6345a2d341ff
samrawit <samri.berih@gmail.com>
2024-12-28 14:36:07 -0800

    worked on

--- backend/app.js
+++ backend/app.js
@@ -69,7 +69,7 @@ app.use((err, _req, _res, next) => {
   next(err);
 });
 
-// Error formatter
+// Error Formatter
 app.use((err, _req, res, _next) => {
   res.status(err.status || 500);
   console.error(err);
@@ -80,5 +80,25 @@ app.use((err, _req, res, _next) => {
     stack: isProduction ? null : err.stack,
   });
 });
+
+// backend/routes/api/index.js
+const router = require('express').Router();
+const sessionRouter = require('./session.js');
+const usersRouter = require('./users.js');
+const { restoreUser } = require("../../utils/auth.js");
+
+// Connect restoreUser middleware to the API router
+  // If current user session is valid, set req.user to the user in the database
+  // If current user session is not valid, set req.user to null
+router.use(restoreUser);
+
+router.use('/session', sessionRouter);
+
+router.use('/users', usersRouter);
+
+router.post('/test', (req, res) => {
+  res.json({ requestBody: req.body });
+});
+
 
 module.exports = app;
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -2,13 +2,27 @@ const express = require('express');
 const router = express.Router();
 const apiRouter = require('./api'); // Import the API router
 
+//const sessionRouter = require('./session.js');
+
 // Connect API routes to `/api`
 router.use('/api', apiRouter);
 
 // Test route
-router.get('/hello/world', (req, res) => {
-  res.cookie('XSRF-TOKEN', req.csrfToken());
-  res.send('Hello World!');
+// router.get('/hello/world', (req, res) => {
+//   res.cookie('XSRF-TOKEN', req.csrfToken());
+//   res.send('Hello World!');
+// });
+
+// backend/routes/index.js
+// ...
+// Add a XSRF-TOKEN cookie
+router.get("/api/csrf/restore", (req, res) => {
+  const csrfToken = req.csrfToken();
+  res.cookie("XSRF-TOKEN", csrfToken);
+  res.status(200).json({
+    'XSRF-Token': csrfToken
+  });
 });
+// ...
 
 module.exports = router;
